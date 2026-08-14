ede7e097467d5dcb002c0550f1bec64e6006fa2b
Hideki Saito <hidekis@gmail.com>
2026-08-13 23:39:05 -0700

    Update front_axis default value to "y+" for improved orientation consistency

--- metaverse_booth_utility/__init__.py
+++ metaverse_booth_utility/__init__.py
@@ -568,7 +568,7 @@ class BoothConfigProperties(PropertyGroup):
     width_m: FloatProperty(name="Width (m)", default=1.0, min=0.1)
     depth_m: FloatProperty(name="Depth (m)", default=1.0, min=0.1)
     height_m: FloatProperty(name="Height (m)", default=1.0, min=0.01)
-    front_axis: EnumProperty(name="Front Axis", items=FRONT_AXIS_ITEMS, default="y-")
+    front_axis: EnumProperty(name="Front Axis", items=FRONT_AXIS_ITEMS, default="y+")
     advanced_open: bpy.props.BoolProperty(name="Advanced", default=False)
 
     def get_config_data(self):
