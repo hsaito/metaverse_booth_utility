b3a13590f0e4c77e697157d5a7840325ddcf9edc
Hideki Saito <hidekis@gmail.com>
2026-08-23 04:00:46 -0700

    Bump version to 1.3.2 in __init__.py and blender_manifest.toml

--- metaverse_booth_utility/__init__.py
+++ metaverse_booth_utility/__init__.py
@@ -903,7 +903,7 @@ bl_info = {
     "name": "Metaverse Booth Utility",
     "blender": (3, 6, 0),
     "category": "Object",
-    "version": (1, 3, 1),
+    "version": (1, 3, 2),
     "author": "Hideki Saito",
     "description": "Generate booth reference frames and front-direction arrows from configurable event presets.",
 }
